f0c6cd37b51bcf16d7be72433e18676a8e87bc2b
wilsonLawler957 <liungwave@gmail.com>
2019-03-03 11:46:07 +0800

    initial commit

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>約跑趣</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
